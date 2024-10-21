364d5eb2141c4e7ebea21f7f682446baaf40ef6e
Samuel <samulbgoncalves@gmail.com>
2024-10-21 00:00:22 -0300

    Criação da página principal

---
+++ projeto/js/script.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.carousel-slide');
+    const totalSlides = slides.length;
+
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+
+    const carousel = document.querySelector('.carousel');
+    carousel.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
+
+// Slide automático (opcional)
+setInterval(() => {
+    moveSlide(1);
+}, 6000); // Troca a cada 5 segundos
+
+    document.getElementById('search-icon').addEventListener('click', function() {
+        var searchBar = document.getElementById('search-bar');
+        if (searchBar.style.display === "none" || searchBar.style.display === "") {
+            searchBar.style.display = "block"; // Mostra a barra de pesquisa
+            searchBar.focus(); // Foca na barra de pesquisa
+        } else {
+            searchBar.style.display = "none"; // Esconde a barra de pesquisa
+        }
+    });
+
